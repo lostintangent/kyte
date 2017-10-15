bbbf5717662ac0569e10370b5dd1f75084e27bda
Jonathan Carter <joncart@microsoft.com>
2017-10-15 00:18:09 -0700

    refactor: Cleaning up the CLI execution logic

--- packages/kyte/lib/cli.js
+++ packages/kyte/lib/cli.js
@@ -1,32 +1,30 @@
+const boxen = require("boxen");
+const opn = require("opn");
+const startSession = require("./startSession");
+const { copy } = require("copy-paste");
+const { cyan, gray, green } = require("chalk");
 const { promisify } = require("util");
 
+const copyToClipboard = promisify(copy);
+
 module.exports = async function(filePath) {
-  const startSession = require("./startSession");
   const { localUrl, tunnelUrl } = await startSession(filePath);
 
-  if (process.stdout.isTTY) {
-    const copyToClipboard = promisify(require("copy-paste").copy);
-    await copyToClipboard(tunnelUrl);
+  await copyToClipboard(tunnelUrl);
 
-    const boxen = require("boxen");
-    const { cyan, gray, green } = require("chalk");
+  const successMessage = [
+    green("New collaborative sesion started!\n"),
+    `${cyan("Local URL:")}  ${localUrl}`,
+    `${cyan("Tunnel URL:")} ${tunnelUrl}\n`,
+    gray("Tunnel URL has been copied to your clipboard")
+  ].join("\n");
 
-    const successMessage = [
-      green("New collaborative sesion started!\n"),
-      `${cyan("Local URL:")}  ${localUrl}`,
-      `${cyan("Tunnel URL:")} ${tunnelUrl}\n`,
-      gray("Tunnel URL has been copied to your clipboard")
-    ];
+  console.log(
+    boxen(successMessage, {
+      margin: { bottom: 1, top: 1 },
+      padding: 1
+    })
+  );
 
-    console.log(
-      boxen(successMessage.join("\n"), {
-        padding: 1,
-        margin: { bottom: 1, top: 1 }
-      })
-    );
-
-    require("opn")(tunnelUrl);
-  } else {
-    console.log(tunnelUrl);
-  }
+  opn(tunnelUrl);
 };
